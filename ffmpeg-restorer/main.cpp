ab82141d79722d5fc562bb28bbcc9b4ad14a5da6
Evgeny Kislov <dev@evgenykislov.com>
2025-01-16 22:38:32 +0300

    Adds command add.

--- ffmpeg-restorer/main.cpp
+++ ffmpeg-restorer/main.cpp
@@ -8,6 +8,7 @@
 const std::string kCommandHelp1 = "help";
 const std::string kCommandHelp2 = "--help";
 const std::string kCommandList = "list";
+const std::string kCommandAdd = "add";
 
 // clang-format off
 
@@ -16,9 +17,13 @@ const char kHelpMessage[] =
     "Usage:\n"
     "  ffmpeg-restorer [command [arguments]]\n"
     "Commands:\n"
+    "  add [ffmpeg arguments] - add new task for convertation\n"
     "  help, --help - print help\n"
     "  list - print list of tasks\n"
-    "Run without command resume tasks, added earlier\n";
+    "Run without command resume tasks, added earlier\n"
+    "Examples:\n"
+    "Add task for video stream copy:\n"
+    "  ffmpeg-restorer add -i input.mp4 -cv copy output.mp4";
 
 // clang-format on
 
@@ -35,35 +40,42 @@ void CommandList() {
 }
 
 
+/*! Обработать все задачи по-очереди */
+void ProcessAllTasks() {
+  auto tasks = Task::GetTasks();
+
+  for (auto it = tasks.begin(); it != tasks.end(); ++it) {
+    Task t;
+    if (t.CreateFromID(*it)) {
+      t.Run();
+    }
+  }
+}
+
+
 int main(int argc, char** argv) {
   if (argc > 1) {
     std::string command = argv[1];
 
-    if (command == kCommandList) {
-      CommandList();
+    if (command == kCommandAdd) {
+      {
+        Task t;
+        if (!t.CreateFromArguments(argc - 2, argv + 2)) {
+          std::cerr << "Can't create task from arguments" << std::endl;
+          return 1;
+        }
+      }
+      ProcessAllTasks();
     } else if (command == kCommandHelp1 || command == kCommandHelp2) {
       PrintHelp();
+    } else if (command == kCommandList) {
+      CommandList();
     }
+
 
     return 0;
   }
 
-
-  if (argc <= 1) {
-    PrintHelp();
-    return 1;
-  }
-
-  // Добавим новое задание
-
-  Task t;
-  if (!t.CreateFromArguments(argc - 1, argv + 1)) {
-    std::cerr << "Can't create task from arguments" << std::endl;
-    return 1;
-  }
-
-  t.Run();
-
-
+  ProcessAllTasks();
   return 0;
 }
